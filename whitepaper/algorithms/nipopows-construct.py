4530c517c1cffbe48746844427fbdf612fb96605
Dionysis Zindros <dionyziz@gmail.com>
2016-12-11 22:36:44 +0000

    Add python pseudocode for NI proof construction

---
+++ whitepaper/algorithms/nipopows-construct.py
@@ -0,0 +1,17 @@
+def ConstructInnerChain(C, i, boundary):
+    B = C[C.length]
+    while B != boundary:
+        blockid = B.interlink[i]
+        B = blockById(blockid)
+        InnerChain.append(B.interlink[i])
+    return InnerChain
+
+def ConstructProof(C, n, interlink):
+    size = C.length - (k - 1)
+    i = C[size].interlink.length
+    Proof[i] = ConstuctInnerChain(DropSuffix(C, k - 1), i, genesis)
+    while i > 0:
+        boundary = min(m, Proof[i].length)
+        i -= 1
+        Proof[i] = ConstructInnerChain(DropSuffix(C, k - 1), i, boundary)
+    return Proof
