44aee519bd542c9d84806723d3eb9ac55e721132
Andrew Miller <socrates1024@gmail.com>
2017-05-15 19:42:38 -0500

    now with marketcaps

--- whitepaper/altcoinsurvey/survey.py
+++ whitepaper/altcoinsurvey/survey.py
@@ -14,9 +14,7 @@ def parse_try(coin):
     except Exception, e:
         broken.append(coin)
         print e
-        
-if not 'coindata' in globals():
-    coindata = {}
+    
 def parse(coin):
     global coindata
     fn = coin + '.html'
@@ -32,7 +30,8 @@ def parse(coin):
         cells = row.findAll("td")
         key = cells[0].findChild('b').contents[0]
         value = cells[1]
-
+        if key.startswith('Symbol'):
+            d["Symbol"] = value.contents[0].strip()
         if key.startswith('Hash'):
             d[key] = value.contents[0].strip()
         if key.startswith('Proof'):
@@ -46,20 +45,57 @@ def parse(coin):
         #print key, value.contents
     print d
 
+if not 'coindata' in globals():
+    coindata = {}
+    for c in coins:
+        parse_try(c)
+
+# Parse
+def parse_marketcap():
+    global market_cap
+    market_cap = {}
+    c = open('coinmarketcap.html').read()
+    a = BS(c)
+    table = a.findAll("table")
+    for row in table[0].findAll("tr"):
+        cells = row.findAll("td")
+        if len(cells) != 10: continue
+        key = cells[2].contents[0]
+        val = cells[3].contents[0].strip()
+        #print cells[0]
+        val = 0 if val == '?' else int(val.replace(',','').replace('$',''))
+        market_cap[key] = val
+
+if not 'market_cap' in globals():
+    parse_marketcap()
+        
 # Spot fixes
 coindata['bytecoin']['Status'] = 'Healthy'
+
+def sample():
+    healthy = [d for d in coindata if 'Status' in coindata[d] and coindata[d]['Symbol'] in market_cap and market_cap[coindata[d]['Symbol']] > 100000]
+
+    print 'healthy:', len(healthy)
+
+    total = float(sum(market_cap[coindata[d]['Symbol']] for d in healthy))
+    print 'total:', total
+    samples = np.random.choice(healthy, 1000, p=[market_cap[coindata[d]['Symbol']]/total for d in healthy])
+    print 'unique:', len(set(samples))
 
 def histog():
     # Assume 80 bytes per header
     import humanize
     from collections import defaultdict
 
-    healthy = [d for d in coindata if 'Status' in coindata[d] and coindata[d]['Status'] == 'Healthy']
-    #healthy = [d for d in coindata if 'Status' in coindata[d]]
+    global healthy
+    #healthy = [d for d in coindata if 'Status' in coindata[d] and coindata[d]['Status'] == 'Healthy']
+    healthy = [d for d in coindata if 'Status' in coindata[d] and coindata[d]['Symbol'] in market_cap and market_cap[coindata[d]['Symbol']] > 100000]
     print 'healthy:', len(healthy)
     
     data = defaultdict(lambda: dict(blocks=0, freq=0, coins=0))
     for d in healthy:
+        symb = coindata[d]['Symbol']
+        print d, coindata[d]['Symbol'], market_cap[symb] if symb in market_cap else '?'
         pp = coindata[d]['Hash Algorithm']
         time = coindata[d]['Block Time']
         try:
