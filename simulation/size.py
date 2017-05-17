49354e93b9ca5bbd8ce39a63636d538c40d39886
Andrew Miller <socrates1024@gmail.com>
2017-05-16 19:16:51 -0500

    Comments in size.py

--- simulation/size.py
+++ simulation/size.py
@@ -1,5 +1,14 @@
 from math import log, ceil
 
+# suffix:
+# C: length of chain
+# x: # of transactions per block
+# m,k: params
+# block_header: prefix blocks 80
+# 48 - header size
+# coinbase transaction: ??
+# hash_size: 32
+
 def nipopow_size(C, x, m, k, block_header_size, suffix_block_header_size, coinbase_size, hash_size):
     mu = ceil(log(C, 2) - log(m, 2))
     count_pi_blocks = m * (mu - 2)
