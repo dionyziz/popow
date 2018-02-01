2db158b59ac48b79efd50a147976a79d8b2223c9
GGLOU <geo_nm@hotmail.com>
2018-02-01 04:17:09 +0000

    Use pythereum 2.3, started implementation of the comparison operator

--- experiment/test_nipopow.py
+++ experiment/test_nipopow.py
@@ -1,6 +1,11 @@
-from ethereum import tester
+#import solc
+from ethereum.tools import tester
 from ethereum import utils
-from ethereum._solidity import get_solidity
+from ethereum.tools._solidity import (
+    get_solidity,
+    compile_file,
+    solidity_get_contract_data
+    )
 SOLIDITY_AVAILABLE = get_solidity() is not None
 
 def sha256(s):
@@ -25,20 +30,37 @@ flatten = lambda x: [z for y in x for z in y]
 
 # Create the simulated blockchain
 
+s = tester.Chain()
 # Need to increase the gas limit. These are some large contracts!
-tester.gas_limit = 314159200
-s = tester.state()
+s.chain.state.gas_limit = 200000000;
 s.mine()
 
 # Create the contract
-contract = s.abi_contract(None, path='./contractNipopow.sol', language='solidity', contract_name='contractNipopow.sol:Nipopow')
+# contract_name='contractNipopow.sol:Nipopow'
+#contract = s.contract('contractNipopow.sol:Nipopow', language='solidity')
 
+contract_path = './contractNipopow.sol'
+contract_name = 'Nipopow'
+contract_compiled = compile_file(contract_path)
+
+contract_data = solidity_get_contract_data(
+    contract_compiled,
+    contract_path,
+    contract_name,)
+
+contract_address = s.contract(contract_data['bin'], language='evm')
+
+contract_abi = tester.ABIContract(
+    s,
+    contract_data['abi'],
+    contract_address)
 
 # Sample block
 sampleBlock = "f8912eb0b65eedfe76e7e63e053d98c02b9cfb03dae5970a86ed85fddf2d8efe020000002717f5042fac9869b9b2ca9284ec87980bd2efc5a33c6353c8158a9ccc62b833aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa00000000ffff7f2000000000".decode('hex')
 
 import cPickle as pickle
 proof = pickle.load(open('proof.pkl'))
+proof_f = pickle.load(open('proof-fork50k.pkl'))
 proof2 = pickle.load(open('proof-2.pkl'))
 proof3 = pickle.load(open('proof-3.pkl'))
 proof4 = pickle.load(open('proof-4.pkl'))
@@ -50,10 +72,11 @@ def str_to_bytes32(s):
         r.append(s[start:start+32])
     return r
 
-def submit_proof(proof=proof):
-
+def extract_headers_siblings(proof = proof):
     headers = []
     siblings = []
+    # mp stands for merkle proof
+    # hs stands for headers. (probably)
     for hs, mp in proof:
         # Copy the header to an array of 4 bytes32
         header = str_to_bytes32(hs)
@@ -65,17 +88,42 @@ def submit_proof(proof=proof):
         #header[3] = chr(len(mp)) + chr(mu) + header[3][2:]
         header[3] = header[3] + ('\x00'*14) + chr(len(mp)) + chr(mu)
         headers.append(header)
-
-        print repr(sha256(sha256(hs)))
+        #print repr(sha256(sha256(hs)))
 
         for (_,sibling) in mp:
             siblings.append(sibling)
 
+    return headers, siblings
+
+# Compare
+def compare_header_proofs(proof1 = proof, proof2 = proof_f):
+
+    headers1, _ = extract_headers_siblings(proof1)
+    headers2, _ = extract_headers_siblings(proof2)
+
+    cnt = 0
+
+    for i in range(0, len(headers1)):
+        for j in range(0, len(headers2)):
+            if headers1[i] == headers2[j]:
+                cnt = cnt + 1
+                print "Match found! ", i, " ", j
+
+    # How many headers are the same in both chains.
+    print cnt
+
+def submit_proof(proof=proof):
+
+    headers, siblings = extract_headers_siblings(proof)
+
     #assert len(sampleBlock) == 112
     #headers = [str_to_bytes32(sampleBlock)]
-    g = s.block.gas_used
-    contract.submit_nipopow(headers, siblings)
-    print 'Gas used:', s.block.gas_used - g
+
+    # Set the gas limit to 2*10^8
+    s.head_state.gas_limit = 200000000
+    g = s.head_state.gas_used
+    contract_abi.submit_nipopow(headers, siblings, startgas = 100000000)
+    print 'Gas used:', s.head_state.gas_used - g
 
 # Take a snapshot before trying out test cases
 #try: s.revert(s.snapshot())
